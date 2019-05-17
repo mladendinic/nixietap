02d6ae067f6d04cb3f3338cfdfcd3296db47a0cd
Mladen Dinic <mladendinic@hotmail.com>
2019-05-17 21:15:25 +0200

    Added dependency file

---
+++ firmware/dependencies.bat
@@ -0,0 +1,6 @@
+pio lib install "Time"
+pio lib install "ArduinoJson@5.13.4"
+pio lib install "ESPAsyncUDP"
+pio lib install https://github.com/tzapu/WiFiManager.git
+pio lib install https://github.com/gmag11/NtpClient.git 
+
